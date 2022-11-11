65b6a3e4f1068c7cc6da7434fca5276528f9030d
Divyabd <55967072+Divyabd@users.noreply.github.com>
2022-11-11 17:32:21 +0530

    Update App.java

--- Maven/src/main/java/com/devopspract/DevopsMaven/App.java
+++ Maven/src/main/java/com/devopspract/DevopsMaven/App.java
@@ -2,13 +2,14 @@ package com.devopspract.DevopsMaven;
 
 /**
  * Hello world!
- *
+ *mwejdbjkwdw
+ *mwejdbjkwdw
  */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello Divya!" );
-        System.out.println( "Hello Divya!" )
+        System.out.println( "Hello Divya!" );
     }
 }
