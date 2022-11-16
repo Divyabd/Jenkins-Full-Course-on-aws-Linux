364899e4521b4ce41d134418e2f912203346f940
divya Gowda <divyagowda8393@gmail.com>
2022-11-16 15:15:41 +0530

    adding terraform  info with commands

---
+++ Terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "mybucketdivyademo"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
