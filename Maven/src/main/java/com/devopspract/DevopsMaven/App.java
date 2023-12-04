0b7a4225b2d83fe80d7e6190202a30c6028c45bc
Divyabd <55967072+Divyabd@users.noreply.github.com>
2023-12-04 08:28:32 +0530

    Update App.java

--- Maven/src/main/java/com/devopspract/DevopsMaven/App.java
+++ Maven/src/main/java/com/devopspract/DevopsMaven/App.java
@@ -9,6 +9,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello MJ , Lavrence,Olafusi,Divya!!!" );
+        System.out.println( "HelloTeam!!!" );
     }
 }
